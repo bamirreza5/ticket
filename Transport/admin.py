de1a60f38aedced72019166d569f17229b4eb4d1
Amirreza <bamirreza5@gmail.com>
2025-07-14 20:12:43 +0330

    add admin for transport

--- Transport/admin.py
+++ Transport/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Transport
 
-# Register your models here.
+@admin.register(Transport)
+class TransportAdmin(admin.ModelAdmin):
+    list_display = ('transport_type', 'origin', 'destination', 'departure_time', 'arrival_time', 'price')
