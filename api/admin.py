ea8c99eae97ce840a8dafd0d6dde534b8e41b9ef
Amirreza <bamirreza5@gmail.com>
2025-07-18 19:53:53 +0330

    fix error

--- api/admin.py
+++ api/admin.py
@@ -8,10 +8,3 @@ class BookingAdmin(admin.ModelAdmin):
     search_fields = ['user__username', 'transport__origin', 'transport__destination', 'seat_number']
     ordering = ['-booking_date']
     
-
-@admin.register(Cancel_Booking)
-class BookingAdmin(admin.ModelAdmin):
-    list_display = ['user', 'transport', 'seat_number', 'booking_date'] 
-    list_filter = ['transport', 'booking_date'] 
-    search_fields = ['user__username', 'transport__origin', 'transport__destination', 'seat_number']
-    ordering = ['-booking_date']  
